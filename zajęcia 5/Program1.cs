1d6a21cd59abe698de0802e51bceb11595bf5e60
Marcin Wiśniewski <61236677+marcinwisniewski98@users.noreply.github.com>
2021-05-30 22:11:43 +0200

    zajęcia 5

---
+++ zajęcia 5/Program1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zajecia5
+{
+    class Palindrome
+    {
+        private string word;
+        private string path;
+
+        public Palindrome(string path)
+        {
+            this.path = path;
+            Console.WriteLine(path);
+        }
+
+        public void readData()
+        {
+            try
+            {
+                FileInfo fi = new FileInfo(path);
+                using (StreamReader sr = fi.OpenText())
+                    {
+                        word = "";
+                        while ((word = sr.ReadLine()) != null)
+                        {
+                            if(checkPalindrome())
+                                Console.WriteLine(word);
+                        }
+                    }
+            }
+            catch (SecurityException) { Console.WriteLine("Permission denied."); }
+            catch (FileNotFoundException) { Console.WriteLine("File not found."); }
+            catch (UnauthorizedAccessException) { Console.WriteLine("Permission denied."); }
+            catch (DirectoryNotFoundException) { Console.WriteLine("The path is incorrect."); }
+        }
+        private bool checkPalindrome()
+        {
+            for (int i = 0; i < word.Length / 2; i++)
+                if (word[i] != word[word.Length - 1 - i])
+                    return false;
+            return true;
+        }
+    }
+    class Program1
+    {
+
+
+        static void Main(string[] args)
+        {
+            Palindrome palindrome = new Palindrome(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\words.txt");
+            palindrome.readData();
+        }
+    }
+}
---
+++ zajęcia 5/Program2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    abstract class First
+    {
+        public abstract void f(int number);
+        public virtual void r(string fullText) {}
+    }
+    class Second : First
+    {
+        public override void f(int number)
+        {
+            List<int> result = new List<int>();
+            int divider = 2;
+            int sqrt = (int)Math.Sqrt(number);
+
+            while (number > 1 && divider < sqrt)
+            {
+                while (number % divider == 0)
+                {
+                    result.Add(divider);
+                    number /= divider;
+                }
+                ++divider;
+            }
+
+            if (number > 1)
+                result.Add(number);
+
+            foreach (int d in result)
+                Console.Write(d + " ");
+            Console.WriteLine();
+        }
+        public override void r(string fullText)
+        {
+            string result = "";
+            for(int i=0; i<fullText.Length; i++)
+            {
+                int count = 1;
+                while (i < fullText.Length - 1 && fullText[i] == fullText[i + 1])
+                {
+                    ++count;
+                    ++i;
+                }
+                if (count > 1)
+                    result += count.ToString() + fullText[i];
+                else
+                    result += fullText[i].ToString();
+            }
+            Console.WriteLine(result);
+        }
+    }
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Second second = new Second();
+            second.f(24);
+            second.r("ccccommmpressss thhhatt");
+        }
+    }
+}
